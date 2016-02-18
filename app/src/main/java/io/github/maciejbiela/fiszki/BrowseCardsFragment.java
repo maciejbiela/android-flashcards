548791593e6c300b2616409a10c2874b9666ec45
Maciej Biela <maciej.biela@icloud.com>
2016-02-18 20:25:21 +0100

    Add category selection for browsing cards

--- app/src/main/java/io/github/maciejbiela/fiszki/BrowseCardsFragment.java
+++ app/src/main/java/io/github/maciejbiela/fiszki/BrowseCardsFragment.java
@@ -7,17 +7,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import io.github.maciejbiela.fiszki.provider.CardsProvider;
 
+public class BrowseCardsFragment extends Fragment
+        implements AdapterView.OnItemSelectedListener {
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class BrowseCardsFragment extends Fragment {
+    @Bind(R.id.sp_category)
+    Spinner spCategory;
 
     @Bind(R.id.cards)
     ListView cards;
@@ -26,16 +27,31 @@ public class BrowseCardsFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_browse_cards, container, false);
         ButterKnife.bind(this, view);
-        Cursor cursor = getContext().getContentResolver().query(CardsProvider.CONTENT_URI, null, null, null, null);
-        CardCursorAdapter adapter = new CardCursorAdapter(getContext(), cursor);
-        cards.setAdapter(adapter);
+        spCategory.setOnItemSelectedListener(this);
+        Cursor cursor = Card.getAll(getContext().getContentResolver());
+        setAdapter(cursor);
         return view;
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String category = spCategory.getSelectedItem().toString();
+        Cursor cursor = Card.getAllForCategory(getContext().getContentResolver(), category);
+        setAdapter(cursor);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    private void setAdapter(Cursor cursor) {
+        CardCursorAdapter adapter = new CardCursorAdapter(getContext(), cursor);
+        cards.setAdapter(adapter);
+    }
 }
--- app/src/main/java/io/github/maciejbiela/fiszki/Card.java
+++ app/src/main/java/io/github/maciejbiela/fiszki/Card.java
@@ -2,12 +2,23 @@ package io.github.maciejbiela.fiszki;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteConstraintException;
 
 import io.github.maciejbiela.fiszki.database.CardsTable;
 import io.github.maciejbiela.fiszki.provider.CardsProvider;
 
 public class Card {
+
+    public static Cursor getAll(ContentResolver contentResolver) {
+        return contentResolver.query(CardsProvider.CONTENT_URI, null, null, null, null);
+    }
+
+    public static Cursor getAllForCategory(ContentResolver contentResolver, String category) {
+        String selection = CardsTable.COLUMN_CATEGORY + " = ?";
+        String[] selectionArgs = {category.toUpperCase()};
+        return contentResolver.query(CardsProvider.CONTENT_URI, null, selection, selectionArgs, null);
+    }
 
     public static boolean add(ContentResolver contentResolver,
                               String inMotherLanguage,
