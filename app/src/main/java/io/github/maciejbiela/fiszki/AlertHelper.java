1c445f454d296e038c8123e76a97ead8e653ccb9
Maciej Biela <maciej.biela@icloud.com>
2016-02-20 22:44:02 +0100

    Add helper for displaying alerts

---
+++ app/src/main/java/io/github/maciejbiela/fiszki/AlertHelper.java
@@ -0,0 +1,21 @@
+package io.github.maciejbiela.fiszki;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class AlertHelper {
+
+    public static void displayAlert(Context context, String alertTitle, String alertMessage) {
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle(alertTitle);
+        alertDialog.setMessage(alertMessage);
+        alertDialog.setButton(DialogInterface.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialog.show();
+    }
+}
